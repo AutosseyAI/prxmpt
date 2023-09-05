4a5167e3ea88e7e0450c9db6d39a17fb5e89df9d
Connor White <connor.bcw@gmail.com>
2023-09-05 09:35:04 -0500

    quote escaping

--- source/components/elements/quotes.tsx
+++ source/components/elements/quotes.tsx
@@ -1,11 +1,22 @@
 import * as Prxmpt from "../../index.js";
 
-export const sq: Prxmpt.PC = (props) => {
-  return <wrap with="'">{props.children}</wrap>;
+export interface QuoteProps {
+  /**
+   * @default false
+   */
+  noEscape?: boolean;
+}
+
+export const sq: Prxmpt.PC<QuoteProps> = (props) => {
+  const text = Prxmpt.render(props.children);
+  const escaped = props.noEscape ? text : text.replace(/'/g, `\\'`);
+  return <wrap with="'">{escaped}</wrap>;
 };
 
-export const dq: Prxmpt.PC = (props) => {
-  return <wrap with='"'>{props.children}</wrap>;
+export const dq: Prxmpt.PC<QuoteProps> = (props) => {
+  const text = Prxmpt.render(props.children);
+  const escaped = props.noEscape ? text : text.replace(/"/g, `\\"`);
+  return <wrap with='"'>{escaped}</wrap>;
 };
 
 export const code: Prxmpt.PC = (props) => {
